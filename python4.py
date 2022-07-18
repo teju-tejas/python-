289573181ca3f54cba0f9e0cf5553cd7485ac4b3
teju-tejas <109363697+teju-tejas@users.noreply.github.com>
2022-07-18 17:06:53 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,13 @@
+#python program to print positive numbers in a list 
+#list of numbers 
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for num in list1 :
+    if num >= 0:
+        print (num,end='')
+for num in list2 :
+     if num >= 0:
+         print (num,end='')
+         
+            
+        
